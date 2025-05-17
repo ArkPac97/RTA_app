c23d732e407af8f8f4056daa3119a03ec02504f8
ArkPac97 <arekpachuta97@gmail.com>
2025-05-17 21:22:59 +0200

    dodanie requirements.txt oraz modyfikacja danych wejściowcyh o nowe pola, modyfikacja procuder.py - określnie dynamicznej ścieżki do wywołania

--- RTA_app-main/kafka/producer.py
+++ RTA_app-main/kafka/producer.py
@@ -1,19 +1,23 @@
 from kafka import KafkaProducer
 import json
 import time
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PRODUCTS_PATH = os.path.join(BASE_DIR, 'data', 'products.json')
 
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-with open('data/products.json', 'r', encoding='utf-8') as f:
+with open(PRODUCTS_PATH, 'r', encoding='utf-8') as f:
     products = json.load(f)
 
 for product in products:
     producer.send('products', product)
     print(f"Sent: {product['title']} - {product['price']} zł")
-    time.sleep(0.2)  # symulacja strumienia
+    time.sleep(4)
 
 producer.flush()
 print("Wysłano wszystkie produkty.")
